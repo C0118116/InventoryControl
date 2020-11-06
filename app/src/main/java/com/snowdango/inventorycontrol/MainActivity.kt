a81ddfab0bfb25b36c3b8e11da0b50bd30372d1a
c01181165c <c01181165c@edu.teu.ac.jp>
2020-11-06 15:58:34 +0900

    ボタン操作

--- app/src/main/java/com/snowdango/inventorycontrol/MainActivity.kt
+++ app/src/main/java/com/snowdango/inventorycontrol/MainActivity.kt
@@ -2,10 +2,29 @@ package com.snowdango.inventorycontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inventoryViewButton = findViewById<Button>(R.id.Button1)
+        val orderViewButton = findViewById<Button>(R.id.Button2)
+        val addItemButton = findViewById<Button>(R.id.Button3)
+
+        inventoryViewButton.setOnClickListener(this)
+        orderViewButton.setOnClickListener(this)
+        addItemButton.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.Button1 -> {
+
+            }
+        }
+
     }
 }
